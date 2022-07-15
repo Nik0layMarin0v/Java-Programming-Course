7219719e1e72ca97456c4026dfa2cf9a6858fb7a
Nikolay Marinov <nikolai.b.marinov@gmail.com>
2022-07-15 23:10:15 +0300

    Added a solution.

---
+++ Programming Basics with Java/Conditional Statements Advanced/Lab/InvalidNumber.java
@@ -0,0 +1,18 @@
+package AdvancedConditionalStatements.Lab;
+
+import java.util.Scanner;
+
+public class InvalidNumber {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int num = Integer.parseInt(scanner.nextLine());
+
+        if (num <= 200 && num >= 100 || num == 0) {
+
+        } else {
+            System.out.println("invalid");
+        }
+
+    }
+}
