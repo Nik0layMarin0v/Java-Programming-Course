91babbf957f40bc8927fea7fb50cb980c57e4a07
Nikolay Marinov <nikolai.b.marinov@gmail.com>
2022-07-19 23:53:30 +0300

    Added a solution.

---
+++ Programming Basics with Java/For Loop/Exercise/Oscars.java
@@ -0,0 +1,31 @@
+package ForLoop.Exercise;
+
+import java.util.Scanner;
+
+public class Oscars {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String actorName = scanner.nextLine();
+        double points = Double.parseDouble(scanner.nextLine());
+        int juryMembers = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 1; i <= juryMembers ; i++) {
+            String juryName = scanner.nextLine();
+            double juryPoints = Double.parseDouble(scanner.nextLine());
+
+            points = points + ((juryName.length() * juryPoints) / 2.0);
+
+            if (points > 1250.5) {
+                break;
+            }
+        }
+
+        if (points >= 1250.5) {
+            System.out.printf("Congratulations, %s got a nominee for leading role with %.1f!", actorName, points);
+        } else {
+            System.out.printf("Sorry, %s you need %.1f more!", actorName, 1250.5 - points);
+        }
+        
+    }
+}
