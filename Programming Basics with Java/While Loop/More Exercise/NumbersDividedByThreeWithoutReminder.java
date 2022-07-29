013c07b634e54f0ccaddfbb8a3d247db026d3f97
Nikolay Marinov <nikolai.b.marinov@gmail.com>
2022-07-29 23:51:07 +0300

    Added a solution.

---
+++ Programming Basics with Java/While Loop/More Exercise/NumbersDividedByThreeWithoutReminder.java
@@ -0,0 +1,12 @@
+package WhileLoop.MoreExercises;
+
+public class NumbersDividedByThreeWithoutReminder {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
