e4a03f595fac95aef8e4b1d5f21cc1a34b30d674
Nikolay Marinov <nikolai.b.marinov@gmail.com>
2022-08-08 00:02:58 +0300

    Added a solution.

---
+++ Programming Basics with Java/Nested Loops/More Exercise/HappyCatParking.java
@@ -0,0 +1,29 @@
+package NestedLoops.MoreExercises;
+
+import java.util.Scanner;
+
+public class HappyCatParking {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int days = Integer.parseInt(scanner.nextLine());
+        int hours = Integer.parseInt(scanner.nextLine());
+        double totalTax = 0;
+
+        for (int i = 1; i <= days; i++) {
+            double dayTax = 0;
+            for (int j = 1; j <= hours; j++) {
+                if (i % 2 == 0 && j % 2 == 1) {
+                    dayTax += 2.5;
+                } else if (i % 2 == 1 && j % 2 == 0) {
+                    dayTax += 1.25;
+                } else {
+                    dayTax += 1;
+                }
+            }
+            System.out.printf("Day: %d - %.2f leva%n", i, dayTax);
+            totalTax += dayTax;
+        }
+        System.out.printf("Total: %.2f leva", totalTax);
+    }
+}
