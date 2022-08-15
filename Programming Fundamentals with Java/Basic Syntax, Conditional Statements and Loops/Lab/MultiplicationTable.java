064f60fa09c1d2218d47d6b0c4f0e33d4794a8d1
Nikolay Marinov <nikolai.b.marinov@gmail.com>
2022-08-15 23:53:01 +0300

    Added a solution.

---
+++ Programming Fundamentals with Java/Basic Syntax, Conditional Statements and Loops/Lab/MultiplicationTable.java
@@ -0,0 +1,15 @@
+package basicSyntaxConditionalStatementsAndLoops.lab;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int num = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 1; i < 11; i++) {
+            System.out.printf("%d X %d = %d%n", num, i, num * i);
+        }
+    }
+}
