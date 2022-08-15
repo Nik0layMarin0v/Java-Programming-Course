b61bc50a6479c2cafb12fa2612160719dc2fd133
Nikolay Marinov <nikolai.b.marinov@gmail.com>
2022-08-15 23:52:14 +0300

    Added a solution.

---
+++ Programming Fundamentals with Java/Basic Syntax, Conditional Statements and Loops/Lab/DivisibleBy3.java
@@ -0,0 +1,15 @@
+package basicSyntaxConditionalStatementsAndLoops.lab;
+
+public class DivisibleBy3 {
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        while (i < 101) {
+            if (i % 3 == 0) {
+                System.out.println(i);
+            }
+            i++;
+        }
+    }
+}
