e6c1b232e94b234607ee646e3c819beb836dd752
Nikolay Marinov <nikolai.b.marinov@gmail.com>
2022-08-15 23:52:33 +0300

    Added a solution.

---
+++ Programming Fundamentals with Java/Basic Syntax, Conditional Statements and Loops/Lab/SumOfOddNumbers.java
@@ -0,0 +1,21 @@
+package basicSyntaxConditionalStatementsAndLoops.lab;
+
+import java.util.Scanner;
+
+public class SumOfOddNumbers {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        int sum = 0;
+        for (int i = 1; i < 2 * n; i++) {
+            if (i % 2 == 1) {
+                System.out.println(i);
+                sum += i;
+            }
+        }
+
+        System.out.println("Sum: " + sum);
+    }
+}
