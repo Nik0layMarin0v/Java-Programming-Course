44006e004845f31806832b5df9145852420c3549
Nikolay Marinov <nikolai.b.marinov@gmail.com>
2022-08-25 18:57:57 +0300

    Added a solution.

---
+++ Programming Fundamentals with Java/Basic Syntax, Conditional Statements and Loops/More Exercise/ReverseString.java
@@ -0,0 +1,16 @@
+package basicSyntaxConditionalStatementsAndLoops.moreExercise;
+
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.nextLine();
+
+        String newWord = "";
+        for (int i = word.length() - 1; i >= 0; i--) {
+            newWord += word.charAt(i);
+        }
+        System.out.println(newWord);
+    }
+}
