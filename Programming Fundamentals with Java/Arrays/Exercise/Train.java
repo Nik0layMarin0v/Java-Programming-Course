8ff316669e887f78cc8fdadbc616eae489182afb
Nikolay Marinov <nikolai.b.marinov@gmail.com>
2022-09-21 23:57:37 +0300

    Added a solution.

---
+++ Programming Fundamentals with Java/Arrays/Exercise/Train.java
@@ -0,0 +1,19 @@
+package arrays.exercise;
+
+import java.util.Scanner;
+
+public class Train {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        int[] people = new int[n];
+
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            people[i] = Integer.parseInt(scanner.nextLine());
+            sum += people[i];
+            System.out.print(people[i] + " ");
+        }
+        System.out.printf("%n%d", sum);
+    }
+}
