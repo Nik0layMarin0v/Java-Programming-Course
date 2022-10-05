51ae361584fda5f90d5fdcbb89cc6704a0331eff
Nikolay Marinov <nikolai.b.marinov@gmail.com>
2022-10-05 19:23:44 +0300

    Added a solution.

---
+++ Programming Fundamentals with Java/Methods/Lab/PrintingTriangle.java
@@ -0,0 +1,26 @@
+package methods.lab;
+
+import java.util.Scanner;
+
+public class PrintingTriangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        pyramid(n);
+    }
+
+    public static void pyramid(int end) {
+        for (int i = 1; i <= end; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+        for (int i = end - 1; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
