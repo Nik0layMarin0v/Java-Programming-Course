237f5498a778bb383544b8553815045a4768019e
Nikolay Marinov <nikolai.b.marinov@gmail.com>
2023-02-21 22:31:36 +0200

    Added a solution.

---
+++ Programming Fundamentals with Java/Text Processing/Exercise/StringExplosion.java
@@ -0,0 +1,25 @@
+package textProcessing.exercise;
+
+import java.util.Scanner;
+
+public class StringExplosion {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        StringBuilder text = new StringBuilder(input);
+
+        int totalStrength = 0;
+        for (int position = 0; position < text.length() ; position++) {
+            char currentSymbol = text.charAt(position);
+            if (currentSymbol == '>') {
+                int attackStrength = Integer.parseInt(text.charAt(position + 1) + ""); //сила на атаката
+                totalStrength += attackStrength;
+            } else if (currentSymbol != '>' && totalStrength > 0) {
+                text.deleteCharAt(position);
+                totalStrength--;
+                position--;
+            }
+        }
+        System.out.println(text);
+    }
+}
