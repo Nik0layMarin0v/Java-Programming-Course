185149a9d3781268e34804ed52e716f2d3d6e3ac
Nikolay Marinov <nikolai.b.marinov@gmail.com>
2023-03-18 23:38:12 +0200

    Added a solution.

---
+++ Programming Fundamentals with Java/Regular Expressions/Exercise/StarEnigma.java
@@ -0,0 +1,10 @@
+package regularExpressions.exercise;
+
+import java.util.Scanner;
+
+public class StarEnigma {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
